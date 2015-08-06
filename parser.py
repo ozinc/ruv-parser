5529b32809ea7958db7557b7c847d3101dd8d0ea
Kári Tristan Helgason <kthelgason@gmail.com>
2015-08-06 14:45:33 +0000

    Replaces print with logging and parse EPG function now actually uses EPG.

--- parser.py
+++ parser.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+import logging as log
+import argparse
 
 import requests
 import bs4 as bs
@@ -10,16 +12,20 @@ AS_RUN_URL = 'http://muninn.ruv.is/files/rstiming/ruv/';
 
 api = OZCoreApi('9f5d3f4900000f5bc8f73db9d677c48478bc09cb', '9f16f362-abad-4042-9e26-a69759347bd9')
 
+log.basicConfig(level=log.DEBUG)
+
 def import_as_run():
-    print('> importing RUV AS-RUN from:', AS_RUN_URL)
+    log.info('> importing RUV AS-RUN from:', AS_RUN_URL)
     r = requests.get(AS_RUN_URL)
     if r.status_code is not 200:
-        raise Exception('Unable to fetch EPG from RUV, status was: {0}'.format(r.status_code))
+        raise Exception('Unable to fetch EPG from RUV, status was: %d', r.status_code)
     soup = bs.BeautifulSoup(r.content, 'xml')
     events = soup.findAll('event')
-    print('> found {0} as run items'.format(len(events)))
+    log.info('> found %d as run items', len(events))
     for event in events:
         collection_id = None
+        # For some reason RUV declares a "materialIdentifier" which is a
+        # combination of serieId and episodeNumber so we have to parse that.
         materialIdentifier = event.find('material-identifier').text
         if materialIdentifier:
             # Get rid of leading zero which RUV prepends to the "material identifier".
@@ -39,22 +45,18 @@ def import_as_run():
         })
 
 def import_epg():
-    print('> importing RUV EPG from:', EPG_URL)
+    log.info('> importing RUV EPG from: %s', EPG_URL)
     r = requests.get(EPG_URL)
     if r.status_code is not 200:
-        raise Exception('Unable to fetch EPG from RUV, status was: {0}'.format(r.status_code))
+        raise Exception('Unable to fetch EPG from RUV, status was: %d', r.status_code)
     soup = bs.BeautifulSoup(r.content, 'xml')
     events = soup.findAll('event')
-    print('> found {0} scheduled items'.format(len(events)))
+    log.info('> found %d scheduled items', len(events))
     for event in events:
-        # For some reason RUV declares a "materialIdentifier" which is a
-        # combination of serieId and episodeNumber so we have to parse that.
-        materialIdentifier = event.find('material-identifier').text
+        # Check if the event is associated with a collection
+        serieId = event.get('serie-id')
         collection_id = None
-        if materialIdentifier:
-            # Get rid of leading zero.
-            serieId = str(int(materialIdentifier.split('-')[0]))
-
+        if serieId:
             # Populate the collection object
             collection = {
                 'externalId': 'ruv_' + serieId,
@@ -62,12 +64,12 @@ def import_epg():
                 'name': event.title.text
             }
             collection_id = import_collection(collection)
-            print('collection_id:', collection_id)
+            log.info('collection_id: %s', collection_id)
 
         # Create the video:
         import_video({
             'title': event.title.text,
-            'externalId': 'ruv_' + event.id.text,
+            'externalId': 'ruv_' + event.get('event-id'),
             'collectionId': collection_id
         })
 
@@ -75,24 +77,24 @@ def import_epg():
 def import_collection(collection):
     external_collection = api.fetch_collection_by_external_id(collection['externalId'])
     if external_collection is None:
-        print('creating collection:', collection)
+        log.info('creating collection:', collection)
         new_collection = api.create_collection(collection)
         return new_collection['id']
     else:
-        print('updating collection:', collection)
+        log.info('updating collection:', collection)
         return external_collection['id']
 
 def import_video(video):
     external_video = api.fetch_video_by_external_id(video['externalId'])
     if external_video is None:
-        print('creating video:', video)
+        log.info('creating video:', video)
         return api.create_video(video)
     else:
-        print('video already existed, doing nothing...')
+        log.info('video already existed, doing nothing...')
         return None
 
 if __name__ == '__main__':
     try:
         import_epg()
     except Exception as e:
-        print(e)
+        log.error(e)
